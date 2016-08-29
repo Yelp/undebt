6c81819da72b7c70769d2a8446a963fe5cf5a3e8
Andrew Mason <andrew@fixedpoint.xyz>
2016-08-29 11:47:32 -0700

    Removes multiprocessing and directory traversal
    Also greatly simplifies CLI

--- undebt/cmd/main.py
+++ undebt/cmd/main.py
@@ -4,10 +4,8 @@ from __future__ import division
 from __future__ import print_function
 
 import argparse
-import multiprocessing
 import os
 import sys
-import time
 import traceback
 
 from undebt.cmd import logger
@@ -58,42 +56,25 @@ def _handle_arguments():
     parser = argparse.ArgumentParser(prog='undebt')
     parser.add_argument(
         'paths', nargs='*', metavar='PATH',
-        help='paths to files or directories (searches for extension recursively) to be modified; '
-        'uses stdin if not passed')
+        help='paths to files to be modified; ' 'uses stdin if not passed',
+    )
     parser.add_argument(
         '--pattern', '-p', metavar='PATH', action='append', required=True,
-        help='paths to pattern definition files')
-    parser.add_argument(
-        '--extension', '-e', metavar='EXT', action='append',
-        help='extensions of files to be modified when searching a directory')
-    parser.add_argument(
-        '--jobs', '-j', metavar='INTEGER', type=int, default=16,
-        help='number of processes to run in parallel (default is 16)')
+        help='paths to pattern definition files',
+    )
     parser.add_argument(
         '--verbose', '-v', action='store_true', default=False,
-        help='verbose logging for troubleshooting')
+        help='verbose logging for troubleshooting',
+    )
     parser.add_argument(
         '--dry-run', '-d', action='store_true', default=False,
-        help='only print to stdout; do not overwrite files')
+        help='only print to stdout; do not overwrite files',
+    )
     return parser.parse_args()
 
 
 @_exit_fail_upon_error
-def _fix_exts(extensions):
-    if extensions is None:
-        return None
-
-    new_exts = []
-    for ext in extensions:
-        if ext.startswith("."):
-            new_exts.append(ext[1:])
-        else:
-            new_exts.append(ext)
-    return new_exts
-
-
-@_exit_fail_upon_error
-def _find_files(paths, extensions):
+def _find_files(paths):
     if paths is None:
         return
 
@@ -101,23 +82,6 @@ def _find_files(paths, extensions):
 
         if os.path.isfile(path):
             yield path
-
-        else:
-            for root, dirs, files in os.walk(path):
-
-                for f in files:
-                    ext = os.path.splitext(f)[-1].lstrip('.')
-
-                    if extensions is None:
-                        log.error('must pass --extension when --input is a directory')
-                        sys.exit(1)
-
-                    if ext in extensions:
-                        yield os.path.join(root, f)
-
-                for d in dirs[:]:
-                    if d != "." * len(d) and d.startswith("."):  # ignore .*
-                        dirs.remove(d)
 
 
 def _process_file(patterns, text_file, dry_run):
@@ -157,57 +121,18 @@ def main():
     args = _handle_arguments()
     logger.setup(args.verbose)  # Reset logging level
 
-    if args.jobs <= 0:
-        log.error('number of processes must be > 0')
-        sys.exit(1)
-
     processor = _file_processor(args.pattern, args.dry_run)
-    files = list(_find_files(args.paths, _fix_exts(args.extension)))
+    files = list(_find_files(args.paths))
 
     if bool(files) != bool(args.paths):
-        log.error('could not find any files for the given paths and extension')
+        log.error('could not find any files for the given paths')
         sys.exit(1)
 
     if not files:  # Single process mode if stdin
         log.info('running in stdin/stdout mode')
         processor(None)
 
-    elif len(files) == 1 or args.jobs == 1:  # Single process if only one file or only one process
-        log.info('running across {} file(s) using a single process'
-                 .format(len(files)))
-        processor(files[0])
-
     else:
-        process_pool = multiprocessing.Pool(args.jobs)
-        try:
-
-            result = process_pool.map_async(
-                processor,
-                files,
-            )
-            process_pool.close()
-
-            log.info('running across {} file(s) using {} processes'
-                     .format(len(files), args.jobs))
-
-            # Cannot do process_pool.wait() because it prevents KeyboardInterrupt from being sent
-            # See http://stackoverflow.com/questions/1408356/keyboard-interrupts-with-pythons-multiprocessing-pool
-            while not result.ready():
-                time.sleep(0.01)
-
-            if not result.successful():
-                log.error('multiprocessing failed (are your replace functions pickleable?)')
-                sys.exit(1)
-
-            result = result.get()
-            assert len(result) == len(files)
-            if not all(result):
-                log.error('failed to process {} files'
-                          .format(len(result) - sum(result)))
-                sys.exit(1)
-
-        except:
-            process_pool.terminate()
-            raise
-        finally:
-            process_pool.join()
+        log.info('running across {} file(s)'.format(len(files)))
+        for f in files:
+            processor(f)
